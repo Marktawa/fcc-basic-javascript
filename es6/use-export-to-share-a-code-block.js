667f3669060f2ba182d65d6db64b95c8b565e4d6
McTawa <markmunyaka@gmail.com>
2021-07-06 21:16:09 +0200

    Added files for ES6 section

---
+++ es6/use-export-to-share-a-code-block.js
@@ -0,0 +1,11 @@
+// Use export to Share a Code Block
+// There are two string-related functions in the editor.
+// Export both of them using the method of your choice.
+
+export const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+export const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
---
+++ es6/write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,12 @@
+// Write Concise Declarative Fnctions with ES6
+// Refactor the fnction setGear inside the object bicycle to use the shorthand syntax described above.
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
