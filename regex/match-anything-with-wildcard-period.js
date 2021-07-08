feca35832e1b33235c4c7ebf774ab8b048feef7d
McTawa <markmunyaka@gmail.com>
2021-07-08 19:51:55 +0200

    Added new files to regex

---
+++ regex/match-anything-with-wildcard-period.js
@@ -0,0 +1,6 @@
+// Match Anything with Wildcard Period
+// Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
