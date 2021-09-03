27375f829371484445c87a58c3c8e9c2dbf7d9b2
McTawa <markmunyaka@gmail.com>
2021-09-03 20:26:21 +0200

    Finished debugging and started basic data structures

---
+++ basic-data-structures/access-an-arrays-contents-using-bracket-notation.js
@@ -0,0 +1,10 @@
+/**Access an Array's Contents Using Bracket Notation
+ * 
+ *  In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+ */
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "Gigacore";
+// Only change code above this line
+console.log(myArray);
---
+++ debugging/understanding-the-differences-between-the-freecodecamp-and-browser-console.js
@@ -0,0 +1,9 @@
+/** Understanding the Differences between the freeCodeCamp and Browser Console
+ * 
+ * First, open your browser console so you can see the logs.
+ */
+
+
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
